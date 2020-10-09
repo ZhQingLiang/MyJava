dff8ac76af431ccc60c81303b03f06095e2e580f
ZhQingLiang <1759492142@qq.com>
2020-10-09 14:22:15 +0800

    git test

---
+++ src/T0.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class T0 {
+    public static void main(String[] args) {
+        String a = "123";
+        String b = a;
+        b = "q";
+        System.out.println(a);
+    }
+}
+
